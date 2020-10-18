3dcb4ad75663eda648a77f3ff76b0a5e0dc254a7
ospacer <995375381@qq.com>
2020-10-18 21:27:23 +0800

    update camera para

--- lib/def/cam_para.h
+++ lib/def/cam_para.h
@@ -1,12 +1,25 @@
 #pragma once
 
+#define USE_IPHONE7P 1
+
 namespace CameraPara
 {
+
+#ifdef USE_STEREO
 
 constexpr double cx = 1004.6;
 constexpr double cy = 253.5;
 constexpr double fx = 1096.6;
 constexpr double fy = 1101.2;
 constexpr double baseline = 4.0496;
+
+#elif USE_IPHONE7P
+
+constexpr double cx = 2016.0;
+constexpr double cy = 1512.0;
+constexpr double fx = 3289.914;
+constexpr double fy = 3289.914;
+
+#endif
 
 } // namespace CameraPara
